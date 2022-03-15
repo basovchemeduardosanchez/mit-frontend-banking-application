6a497a7520e9f75febabf22a10fb368d5e0b8f9d
Eduardo Sánchez Villafán <basov_chem_eduardo_sanchez@hotmail.com>
2022-03-15 06:37:35 -0600

    fix: comment pages not in the wireframes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import { UserContext } from './context';
 import { NavBar } from './navbar';
 import { Home } from './home';
 import { CreateAccount } from './createaccount';
-import { Login } from './login';
+// import { Login } from './login';
 import { Deposit } from './deposit';
 import { Withdraw } from './withdraw';
-import { Balance } from './balance';
+// import { Balance } from './balance';
 import { AllData } from './alldata';
 
 // SECTION Import Bootstrap
@@ -28,10 +28,10 @@ function App() {
                     <div className="container" style={{padding: '20px'}}>
                         <Route path="/" exact component={Home} />
                         <Route path="/CreateAccount/" component={CreateAccount} />
-                        <Route path="/login/" component={Login} />
+                        { /*<Route path="/login/" component={Login} />*/}
                         <Route path="/deposit/" component={Deposit} />
                         <Route path="/withdraw/" component={Withdraw} />
-                        <Route path="/balance/" component={Balance} />
+                        { /* <Route path="/balance/" component={Balance} />*/ }
                         <Route path="/alldata/" component={AllData} />
                     </div>
                 </UserContext.Provider>
