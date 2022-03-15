d300f2bf4bf7e87ec930084101766e66d11b41e4
Eduardo Sánchez Villafán <basov_chem_eduardo_sanchez@hotmail.com>
2022-03-15 08:27:46 -0600

    feat: complete alldata page

--- src/alldata.js
+++ src/alldata.js
@@ -1,14 +1,39 @@
-import React from 'react';
-
-import { useContext } from 'react';
-import { UserContext } from './context';
+import React, { useContext } from 'react';
+import { Card, UserContext } from './context';
 
 export function AllData(){
     const ctx = useContext(UserContext);
     return (
         <>
             <h5>All Data in Store</h5>
-            {JSON.stringify(ctx)}<br/>
+            <Card
+                fullWidth={true}
+                txtcolor="black"
+                body={
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Email</th>
+                                <th>Name</th>
+                                <th>Password</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                ctx.users.map( ( pUser, pIndex ) => {
+                                    return (
+                                        <tr key={pIndex}>
+                                            <td>{pUser.email}</td>
+                                            <td>{pUser.name}</td>
+                                            <td>{pUser.password}</td>
+                                        </tr>
+                                    );
+                                } )
+                            }
+                        </tbody>
+                    </table>
+                }
+            ></Card>
         </>
     );
 }
