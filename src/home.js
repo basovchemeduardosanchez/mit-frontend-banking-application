04a38e1d4e72486e2e6c45d00640d6b466f9b2ec
Eduardo Sánchez Villafán <basov_chem_eduardo_sanchez@hotmail.com>
2022-03-15 06:36:25 -0600

    fix: modify home page according to the wireframes

--- src/home.js
+++ src/home.js
@@ -8,9 +8,8 @@ export function Home(){
     return (
         <Card
             txtcolor="black"
-            header="BadBank Landing Module"
             title="Welcome to the bank"
-            text="You can move around using the navigation bar."
+            text="For all your banking needs"
             body={(<img src={ bankImage } className="img-fluid" alt="Responsive image"/>)}
         />
     );
